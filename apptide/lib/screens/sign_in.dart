d7054a2bcbfc4f0e0cacf0081e02219e70c84794
Augustus onyekachi <onyekachiaugustus111@gmail.com>
2021-09-08 00:59:12 +0100

    last update

--- apptide/lib/screens/sign_in.dart
+++ apptide/lib/screens/sign_in.dart
@@ -12,6 +12,7 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
